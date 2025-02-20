7332faf173b480fd33e75c9d2e550da07e7f8bec
Rasmus Hanzén <rhz1994@egmail.com>
2025-02-20 10:58:17 +0100

    saveText

--- src/components/Textfield.vue
+++ src/components/Textfield.vue
@@ -1,20 +1,37 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 
-const userText = ref("")
+const userText = ref("");
 
-function onFormSubmit() {
-    console.log("submitted", userText)
+function saveText() {
+  if (!newText.value.trim()) return;
+
+  const savedText = {
+    id: Date.now(),
+    text: newText.value,
+    date: new Date().toLocaleDateString("se-SV"),
+  };
+
+  const storedTexts = JSON.parse(localStorage.getItem("savedTexts")) || [];
+  storedTexts.push(savedText);
+  savedEntries.value = storedTexts;
+  localStorage.setItem("savedTexts", JSON.stringify(storedTexts));
+  newText.value = "";
 }
-
 </script>
 
 <template>
-    <form @submit.prevent="onFormSubmit" class="flex flex-col gap-4">
-        <div class="flex flex-col gap-1">
-          <textarea v-model="userText" placeholder="Write whatever you want. Your goal is to write for 2 minutes, but don't limit yourself." name="userText" rows="10" cols="1"> </textarea>
-      </div>
-      <button type="submit" severity="secondary" label="Save text"></button>
+  <form @submit.prevent="saveText" class="flex flex-col gap-4">
+    <div class="flex flex-col gap-1">
+      <textarea
+        v-model="userText"
+        placeholder="Write whatever you want. Your goal is to write for 2 minutes, but don't limit yourself."
+        name="userText"
+        rows="10"
+        cols="1"
+      >
+      </textarea>
+    </div>
+    <button type="submit" severity="secondary" label="Save text"></button>
   </form>
-
 </template>
