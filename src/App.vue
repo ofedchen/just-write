55286b8a356e5d934e729be0e915b55b021d8eb7
Olha Fedchenko <ofedchen@gmail.com>
2025-02-20 11:50:02 +0100

    fixed styling for HomeView

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,20 @@ import PromptGenerator from "./components/PromptGenerator.vue";
 </script>
 
 <template>
-  <RouterLink to='/'><h1 class="font-[Special_Elite] text-[42px] text-center py-8">Just Write</h1></RouterLink>
-  <nav>
-    <ul>
-      <li>
-        <!-- <RouterLink to="/">Hem</RouterLink> -->
-      </li>
-    </ul>
-  </nav>
+    <RouterLink to='/'>
+      <h1 class="font-[Special_Elite] text-[42px] text-center py-8">Just Write</h1>
+    </RouterLink>
+    <nav>
+      <ul>
+        <li>
+          <!-- <RouterLink to="/">Hem</RouterLink> -->
+        </li>
+      </ul>
+    </nav>
     <RouterView />
 </template>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Special+Elite&display=swap");
 @import "tailwindcss";
-
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,8 @@ import Textfield from "../components/Textfield.vue";
 </script>
 
 <template>
-  <main>
     <main class="grid grid-cols-1 md:grid-cols-3 gap-4 px-8">
       <PromptGenarator />
       <Textfield class="md:col-span-2" />
     </main>
-  </main>
 </template>
