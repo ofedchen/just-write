3c4df04807b9378e9b007e7f5fc384846ab41e32
Rasmus Hanzén <rhz1994@egmail.com>
2025-03-10 13:09:31 +0100

    added userform on json

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,32 +1,40 @@
 <script setup>
   import { RouterLink, RouterView } from "vue-router";
-  import { onMounted, ref, watch } from "vue";
+  import { onMounted, ref, watch, computed } from "vue";
   // import { useRoute } from "vue-router";
   import { UserIcon } from "@heroicons/vue/24/solid";
+  import { useToast } from "vue-toastification";
+
   import axios from "axios";
   import { useInlogStatus } from "/src/store/";
-  import { useProfileStore } from "/src/storeProfile/";
+  // import { useProfileStore } from "/src/storeProfile/";
   import TextDisplayed from "../components/TextDisplayed.vue";
 
   const inlog = useInlogStatus();
-  const profile = useProfileStore();
+  const toast = useToast();
+  // const profile = useProfileStore();
   const publishedTexts = ref([]);
   const likedTexts = ref([]);
   const expandedText = ref({});
+  const firstname = ref("");
+  const surname = ref("");
   const bioText = ref("");
   const authorText = ref("");
   const genreText = ref("");
   const bookText = ref("");
-  const bioSaved = ref(false);
+  // const bioSaved = ref(false);
   const displayLikedTexts = ref(false);
+  const userInput = ref([]);
+  const foundUserInput = ref(false);
+  const filterUserInput = ref([]);
+  const id = ref("");
 
   onMounted(async () => {
-    bioSaved.value = true;
+    foundUserInput.value = true;
 
     try {
       const response = await axios.get(`/api/publishedTexts`);
       publishedTexts.value = response.data;
-      console.log(publishedTexts.value);
     } catch (error) {
       console.error("Error fetching texts", error);
     }
@@ -36,94 +44,151 @@
     likedTexts.value = newPublishedTexts.filter((text) =>
       text.likesList.includes(inlog.user)
     );
-    console.log("Filtered texts:", likedTexts.value);
   });
 
-  const saveBio = () => {
-    profile.bio = bioText.value;
-    profile.favoriteAuthors = authorText.value;
-    profile.favoriteGenres = genreText.value;
-    profile.favoriteBook = bookText.value;
-
-    bioSaved.value = true;
-  };
   const editBio = () => {
-    bioSaved.value = false;
+    foundUserInput.value = false;
   };
   function readMoreLess(id) {
     // function to expand and minimize saved texts
     expandedText.value[id] = !expandedText.value[id];
   }
 
-  function showLikedTexts() {
-    displayLikedTexts.value = !displayLikedTexts.value;
+  // Skicka användarens input
+
+  // Hämta och logga användardata för att säkerställa att den finns
+
+  async function sendUserForm() {
+    const form = {
+      user: inlog.user,
+      firstname: firstname.value,
+      surname: surname.value,
+      profileBio: bioText.value,
+      profileFavoriteAuthors: authorText.value,
+      profileFavoriteGenres: genreText.value,
+      profileFavoriteBook: bookText.value
+    };
+    foundUserInput.value = true;
+
+    id.value = filterUserInput.value[0].id;
+    try {
+      await axios({
+        method: "patch",
+        url: `/api/userForm/${id.value}`,
+        data: form
+      });
+    } catch (error) {
+      console.error("Error filling form", error);
+      toast.error("User form has not been filled");
+    }
+    fetchUserForm();
   }
+  // hämta användarens input
+  const fetchUserForm = async () => {
+    try {
+      const response = await axios.get(`/api/userForm`);
+      userInput.value = response.data;
+
+      filterUserInput.value = userInput.value.filter((input) =>
+        input.user.includes(inlog.user)
+      );
+
+      if (filterUserInput.value > 0) {
+        foundUserInput.value = true;
+      }
+    } catch (error) {
+      console.error("Error fetching userForm", error);
+    }
+  };
+
+  onMounted(async () => {
+    // Hämta användardata när komponenten laddas
+    await fetchUserForm();
+
+    try {
+      const response = await axios.get(`/api/publishedTexts`);
+      publishedTexts.value = response.data;
+    } catch (error) {
+      console.error("Error fetching texts", error);
+    }
+  });
 </script>
 
 <template>
   <div class="bg-gray-800 w-full h-50 absolute top-35 left-0">
     <UserIcon class="h-30 w-30 border mt-35 bg-white rounded-full ml-20" />
-
-    <div class="ml-20 mt-10 text-lg">
-      <!-- <p>{{ inlog.firstname }} {{ inlog.surname }}</p> -->
-    </div>
   </div>
-
-  <form
-    class="flex flex-col mt-60 w-50 mb-10"
-    @submit.prevent="saveBio"
-    action=""
-    v-if="!bioSaved"
-  >
-    <label>Tell us something about yourself </label>
-    <input v-model="bioText" class="border border-gray-800" type="text" />
-    <label>Favourite author? </label>
-    <input v-model="authorText" class="border border-gray-800" type="text" />
-    <label>Favourite genre? </label>
-    <input v-model="genreText" class="border border-gray-800" type="text" />
-    <label>Favourite book? </label>
-    <input v-model="bookText" class="border border-gray-800" type="text" />
-    <button class="bg-gray-800 mt-2 text-white cursor-pointer rounded-sm p-2">
-      Save
-    </button>
-  </form>
-  <div v-if="bioSaved" class="flex flex-col mt-60 w-50 mb-10">
-    <label>About me </label>
-
-    <p class="border-b border-gray-800 p-3">{{ profile.bio }}</p>
-    <label>Favourite author? </label>
-
-    <p class="border-b border-gray-800 p-3">{{ profile.favoriteAuthors }}</p>
-    <label>Favourite genre? </label>
-
-    <p class="border-b border-gray-800 p-3">{{ profile.favoriteGenres }}</p>
-    <label>Favourite book? </label>
-
-    <p class="border-b border-gray-800 p-3">{{ profile.favoriteBook }}</p>
-
-    <button
-      class="bg-gray-800 mt-2 text-white cursor-pointer rounded-sm p-2"
-      @click="editBio"
+  <div>
+    <form
+      class="flex flex-col mt-60 w-50 mb-10"
+      @submit.prevent="sendUserForm"
+      action=""
+      v-if="!foundUserInput"
     >
-      Edit Bio
-    </button>
-  </div>
+      <label>Firstname </label>
+      <input v-model="firstname" class="border border-gray-800" type="text" />
+      <label>Surname </label>
+      <input v-model="surname" class="border border-gray-800" type="text" />
+      <label>Tell us something about yourself </label>
+      <input v-model="bioText" class="border border-gray-800" type="text" />
+      <label>Favourite author? </label>
+      <input v-model="authorText" class="border border-gray-800" type="text" />
+      <label>Favourite genre? </label>
+      <input v-model="genreText" class="border border-gray-800" type="text" />
+      <label>Favourite book? </label>
+      <input v-model="bookText" class="border border-gray-800" type="text" />
+      <button
+        @click="sendUserForm"
+        class="bg-gray-800 mt-2 text-white cursor-pointer rounded-sm p-2"
+      >
+        Save
+      </button>
+    </form>
+    <div v-if="foundUserInput" class="flex flex-col mt-60 w-50 mb-10">
+      <p class="border-b border-gray-800 p-3">
+        {{ filterUserInput[0].firstname || "" }}
+      </p>
 
-  <button
-    @click="showLikedTexts"
-    class="text-lg bg-gray-800 mt-2 text-white cursor-pointer rounded-sm p-2"
-  >
-    Look at liked texts
-  </button>
+      <p class="border-b border-gray-800 p-3">
+        {{ filterUserInput[0].surname || "" }}
+      </p>
+      <label>About me </label>
 
-  <TextDisplayed
-    v-if="displayLikedTexts"
-    :expanded-text="expandedText"
-    :texts="likedTexts"
-    @expand="readMoreLess"
-  />
+      <p class="border-b border-gray-800 p-3">
+        {{ filterUserInput[0].profileBio || "" }}
+      </p>
+      <label>Favourite author? </label>
 
-  <!-- <RouterLink to="/savedtexts" class="ml-auto">
+      <p class="border-b border-gray-800 p-3">
+        {{ filterUserInput[0].profileFavoriteAuthors || "" }}
+      </p>
+      <label>Favourite genre? </label>
+
+      <p class="border-b border-gray-800 p-3">
+        {{ filterUserInput[0].profileFavoriteGenres || "" }}
+      </p>
+      <label>Favourite book? </label>
+
+      <p class="border-b border-gray-800 p-3">
+        {{ filterUserInput[0].profileFavoriteBook || "" }}
+      </p>
+
+      <button
+        class="bg-gray-800 mt-2 text-white cursor-pointer rounded-sm p-2"
+        @click="editBio"
+      >
+        Edit Bio
+      </button>
+    </div>
+
+    <TextDisplayed
+      :expanded-text="expandedText"
+      :texts="likedTexts"
+      @expand="readMoreLess"
+    />
+
+    <!-- <RouterLink to="/savedtexts" class="ml-auto">
       <h2 class="font-[Overpass] text-[20px]">My saved writings</h2>
     </RouterLink> -->
+  </div>
 </template>
